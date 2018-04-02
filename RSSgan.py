72de3e1dc632a3655710a83627098996443c282a
noob_saibot <halo2olah90@gmail.com>
2018-04-02 23:11:02 +0300

    Non-linear activation for output layer + restore saved model and evaluate.

--- RSSgan.py
+++ RSSgan.py
@@ -16,54 +16,54 @@ df = con.get_values('proc_table')
 data = df.drop('name', axis=1).values
 
 
-batch_size = 10
+batch_size = 20
 updates = 40000
-learning_rate = 0.001
+learning_rate = 0.01
 prior_mu = -2.5
 prior_std = 0.5
 noise_range = 5
 
 
 gen_weights = {
-    'w1': tf.get_variable("gw1", shape=[data.shape[1], 10],
+    'w1': tf.get_variable("gw1", shape=[data.shape[1], 30],
                           initializer=tf.keras.initializers.he_normal()),
-    'b1': tf.Variable(tf.random_normal([10])),
-    'w2': tf.get_variable("gw2", shape=[10, data.shape[1]],
+    'b1': tf.Variable(tf.random_normal([1])),
+    'w2': tf.get_variable("gw2", shape=[30, 10],
                           initializer=tf.keras.initializers.he_normal()),
     'b2': tf.Variable(tf.random_normal([1])),
-    # 'w3': tf.get_variable("gw3", shape=[10, data.shape[1]],
-    #                       initializer=tf.keras.initializers.he_normal()),
-    # 'b3': tf.Variable(tf.random_normal([1])),
+    'w3': tf.get_variable("gw3", shape=[10, data.shape[1]],
+                          initializer=tf.keras.initializers.he_normal()),
+    'b3': tf.Variable(tf.random_normal([1])),
 }
 
 disc_weights = {
-    'w1': tf.get_variable("dw1", shape=[data.shape[1], 10],
+    'w1': tf.get_variable("dw1", shape=[data.shape[1], 3],
                           initializer=tf.contrib.layers.xavier_initializer()),
-    'b1': tf.Variable(tf.random_normal([10])),
-    'w2': tf.get_variable("dw2", shape=[10, 5],
+    'b1': tf.Variable(tf.random_normal([1])),
+    'w2': tf.get_variable("dw2", shape=[3, 5],
                           initializer=tf.contrib.layers.xavier_initializer()),
-    'b2': tf.Variable(tf.random_normal([5])),
+    'b2': tf.Variable(tf.random_normal([1])),
     'w3': tf.get_variable("dw3", shape=[5, data.shape[1]],
                           initializer=tf.contrib.layers.xavier_initializer()),
     'b3': tf.Variable(tf.random_normal([1])),
 }
 
 
-z_p = tf.placeholder('float', [None, data.shape[1]])
+z_p = tf.placeholder('float', [None, data.shape[1]], name='z_p')
 x_d = tf.placeholder('float', [None, data.shape[1]])
-g_h1 = tf.nn.relu(tf.add(tf.matmul(z_p, gen_weights['w1']), gen_weights['b1']))
-# g_h2 = tf.nn.softplus(tf.add(tf.matmul(g_h1, gen_weights['w2']), gen_weights['b2']))
+g_h1 = tf.nn.leaky_relu(tf.add(tf.matmul(z_p, gen_weights['w1']), gen_weights['b1']))
+g_h2 = tf.nn.leaky_relu(tf.add(tf.matmul(g_h1, gen_weights['w2']), gen_weights['b2']))
 
 # batch normalization
 scale1 = tf.Variable(tf.ones([10]))
 beta1 = tf.Variable(tf.zeros([10]))
-batch_mean2, batch_var2 = tf.nn.moments(g_h1,[0])
+batch_mean2, batch_var2 = tf.nn.moments(g_h2,[0])
 scale2 = tf.Variable(tf.ones([10]))
 beta2 = tf.Variable(tf.zeros([10]))
 epsilon = 1e-3
 
-g_h2 = tf.nn.batch_normalization(g_h1, batch_mean2,batch_var2,beta2,scale2,epsilon)
-x_g = tf.add(tf.matmul(g_h2, gen_weights['w2']), gen_weights['b2'])
+g_h2 = tf.nn.batch_normalization(g_h2, batch_mean2,batch_var2,beta2,scale2,epsilon)
+x_g = tf.nn.leaky_relu(tf.add(tf.matmul(g_h2, gen_weights['w3']), gen_weights['b3'], name='out_gen'))
 
 h1gw = tf.summary.histogram("weights_g1", gen_weights['w1'])
 h1gb = tf.summary.histogram("biases_g1", gen_weights['b1'])
@@ -110,12 +110,12 @@ tf.summary.scalar("cost_function_G", G_cost)
 tf.summary.scalar("cost_function_D", D_cost)
 
 
-optimizer = tf.train.AdamOptimizer(learning_rate)
+optimizer = tf.train.AdagradOptimizer(learning_rate)
 D_optimizer = optimizer.minimize(D_cost, var_list=list(disc_weights.values()))
 G_optimizer = optimizer.minimize(G_cost, var_list=list(gen_weights.values()))
 
 def sample_z(size=batch_size):
-    return np.random.uniform(-noise_range, noise_range, size=[size, data.shape[1]])
+    return np.random.normal(-noise_range, noise_range, size=[size, data.shape[1]])
 
 def sample_x(size=batch_size, mu=prior_mu, std=prior_std):
     return np.random.normal(mu, std, size=[size, 1])
@@ -127,6 +127,7 @@ def sample_data(data, size=batch_size):
 merged_summary_op = tf.summary.merge_all()
 
 init = tf.global_variables_initializer()
+saver = tf.train.Saver()
 sess = tf.Session(config=tf.ConfigProto())
 sess.run(init)
 
@@ -139,23 +140,23 @@ for i in range(updates):
     x_batch = sample_data(data)
 
     # Forced slow down updates of discriminator
-    counter += 1
-    if counter == 2:
-        test_z = sample_z(10)
-        test_x = sample_data(data, 10)
-        cost1 = sess.run(D_cost, feed_dict={z_p: test_z, x_d: test_x})
-        if cost1 > 0.6:
-            sess.run(D_optimizer, feed_dict={z_p: z_batch, x_d: x_batch})
-        counter = 0
-    # sess.run(D_optimizer, feed_dict={z_p: z_batch, x_d: x_batch})
+    # counter += 1
+    # if counter == 2:
+    #     test_z = sample_z(10)
+    #     test_x = sample_data(data, 10)
+    #     cost1 = sess.run(D_cost, feed_dict={z_p: test_z, x_d: test_x})
+    #     if cost1 > 1.2:
+    #         sess.run(D_optimizer, feed_dict={z_p: z_batch, x_d: x_batch})
+    #     counter = 0
+    sess.run(D_optimizer, feed_dict={z_p: z_batch, x_d: x_batch})
 
     z_batch = sample_z()
     sess.run(G_optimizer, feed_dict={z_p: z_batch})
 
-    if i % 1000 == 0:
+    if i % 5000 == 0:
         print('Step: ', i)
-        test_z = sample_z(10000)
-        test_x = sample_data(data, 10000)
+        test_z = sample_z(5000)
+        test_x = sample_data(data, 5000)
 
         cost1 = sess.run(D_cost, feed_dict={z_p: test_z, x_d: test_x})
         cost2 = sess.run(G_cost, feed_dict={z_p: test_z})
@@ -165,4 +166,6 @@ for i in range(updates):
 
     summary_str = sess.run(merged_summary_op, feed_dict={z_p: z_batch, x_d: x_batch})
     summary_writer.add_summary(summary_str, i * batch_size + i)
+saver.save(sess, './logs/my_test_model')
+del sess
 
--- tmp.py
+++ tmp.py
@@ -1,81 +1,29 @@
-# Import MNIST data
-from tensorflow.examples.tutorials.mnist import input_data
-
-mnist = input_data.read_data_sets("/tmp/data/", one_hot=True)
-
 import tensorflow as tf
+import numpy as np
 
-# Set parameters
-learning_rate = 0.01
-training_iteration = 30
-batch_size = 100
-display_step = 2
+batch_size = 10
+updates = 4000
+learning_rate = 0.001
+prior_mu = -2.5
+prior_std = 0.5
+noise_range = 5
+data_shape = 507
 
-# TF graph input
-x = tf.placeholder("float", [None, 784])  # mnist data image of shape 28*28=784
-y = tf.placeholder("float", [None, 10])  # 0-9 digits recognition => 10 classes
+def sample_z(size=1):
+    return np.random.normal(-noise_range, noise_range, size=[size, data_shape])
 
-# Create a model
+graph = tf.get_default_graph()
 
-# Set model weights
-W = tf.Variable(tf.zeros([784, 10]))
-b = tf.Variable(tf.zeros([10]))
+with tf.Session(graph=graph) as sess:
+    new_saver = tf.train.import_meta_graph('./logs/my_test_model.meta')
+    new_saver.restore(sess, tf.train.latest_checkpoint('./logs/'))
+    # test_z = tf.convert_to_tensor(sample_z(1))
+    test_z = sample_z(2)
 
-with tf.name_scope("Wx_b") as scope:
-    # Construct a linear model
-    model = tf.nn.softmax(tf.matmul(x, W) + b)  # Softmax
-
-# Add summary ops to collect data
-w_h = tf.summary.histogram("weights", W)
-b_h = tf.summary.histogram("biases", b)
-
-# More name scopes will clean up graph representation
-with tf.name_scope("cost_function") as scope:
-    # Minimize error using cross entropy
-    # Cross entropy
-    cost_function = -tf.reduce_sum(y * tf.log(model))
-    # Create a summary to monitor the cost function
-    tf.summary.scalar("cost_function", cost_function)
-
-with tf.name_scope("train") as scope:
-    # Gradient descent
-    optimizer = tf.train.GradientDescentOptimizer(learning_rate).minimize(cost_function)
-
-# Initializing the variables
-init = tf.global_variables_initializer()
-
-# Merge all summaries into a single operator
-merged_summary_op = tf.summary.merge_all()
-
-# Launch the graph
-with tf.Session() as sess:
-    sess.run(init)
-
-    # Set the logs writer to the folder /tmp/tensorflow_logs
-    summary_writer = tf.summary.FileWriter('logs', graph_def=tf.GraphDef())
-
-    # Training cycle
-    for iteration in range(training_iteration):
-        avg_cost = 0.
-        total_batch = int(mnist.train.num_examples / batch_size)
-        # Loop over all batches
-        for i in range(total_batch):
-            batch_xs, batch_ys = mnist.train.next_batch(batch_size)
-            # Fit training using batch data
-            sess.run(optimizer, feed_dict={x: batch_xs, y: batch_ys})
-            # Compute the average loss
-            avg_cost += sess.run(cost_function, feed_dict={x: batch_xs, y: batch_ys}) / total_batch
-            # Write logs for each iteration
-            summary_str = sess.run(merged_summary_op, feed_dict={x: batch_xs, y: batch_ys})
-            summary_writer.add_summary(summary_str, iteration * total_batch + i)
-        # Display logs per iteration step
-        if iteration % display_step == 0:
-            print("Iteration:", '%04d' % (iteration + 1), "cost=", "{:.9f}".format(avg_cost))
-
-    print("Tuning completed!")
-
-    # Test the model
-    predictions = tf.equal(tf.argmax(model, 1), tf.argmax(y, 1))
-    # Calculate accuracy
-    accuracy = tf.reduce_mean(tf.cast(predictions, "float"))
-    print("Accuracy:", accuracy.eval({x: mnist.test.images, y: mnist.test.labels}))
+    # print(graph.get_name_scope())
+    # input = graph.get_operation_by_name("z_p").outputs[0]
+    # prediction = graph.get_operation_by_name("x_g").outputs[0]
+    z_p = graph.get_tensor_by_name("z_p:0")
+    op_to_restore = graph.get_tensor_by_name("out_gen:0")
+    # newdata=put your data here
+    print(sess.run(op_to_restore, feed_dict={z_p: test_z}).tolist())
